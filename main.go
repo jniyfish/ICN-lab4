9c1e27b3f9460cef11175989d570d964f5e26785
jniyfish <cry589036511@gmail.com>
2021-03-27 22:09:49 -0700

    ff

--- main.go
+++ main.go
@@ -14,11 +14,12 @@ import (
 )
 
 var (
-    snapshot_len int32  = 1024
+    snapshot_len int32  = 65536
     promiscuous  bool   = false
     err          error
     timeout      time.Duration = 0 * time.Second
     handle       *pcap.Handle
+    filter       string =""
     x = 1
 )
 
@@ -40,16 +41,25 @@ func main() {
     // Open device
     handle, err = pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
     if err != nil {log.Fatal(err) }
-    defer handle.Close()
-
-    // Use the handle as a packet source to process all packets
-    packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+        defer handle.Close()
+    
+    fmt.Println("Insert BPF filter expression: ")
+    fmt.Scanln(&filter)
+    filter ="ip proto gre"
+    fmt.Printf("filter: %s\n", filter)
     pnum:=1
-    for packet := range packetSource.Packets() {
-        // Process packet here
-        fmt.Printf("\nPacket Num [%d]\n", pnum)
-        pnum = pnum+1
-        printPacketInfo(packet)
+    for true {
+        handle, err = pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
+        err = handle.SetBPFFilter(filter)
+        packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+        for packet := range packetSource.Packets() {
+            // Process packet here
+            fmt.Printf("\nPacket Num [%d]\n", pnum)
+            pnum = pnum+1
+            printPacketInfo(packet)
+            handle.Close()
+            break;
+        }
     }
 }
 
@@ -103,15 +113,22 @@ func printPacketInfo(packet gopacket.Packet) {
         fmt.Println("%d",isGre)
     }
     if len(byte) >= 46 {
-        //fmt.Println(byte)
-        //fmt.Println(byte[44])
-        //fmt.Println(byte[45])
         if byte[44] == 101 && byte[45] == 88 {
             isGre = 1
             fmt.Println("Next Layer Protocol: GRE")
         }
     }
     if isGre == 1 && src!="140.113.0.1"{
+        
+        if src == "140.114.0.1"{
+            filter = "not host 140.114.0.1 and ip proto gre"
+        } else if src == "140.115.0.1"{
+            filter = "not host 140.115.0.1 and ip proto gre"
+        } else if src == "140.114.0.1" && (filter == "not host 140.115.0.1 and ip proto gre") {
+            filter = "not host (140.114.0.1 and 140.115.0.1) and ip proto gre"
+        } else if src == "140.115.0.1" && (filter == "not host 140.114.0.1 and ip proto gre"){
+            filter = "not host (140.114.0.1 and 140.115.0.1) and ip proto gre"
+        }
         vx:= "gre_iface" + strconv.Itoa(x)
         app:= "ip"
         link:="link"
@@ -124,21 +141,19 @@ func printPacketInfo(packet gopacket.Packet) {
         local:="local"
         master:="master"
         br0:="br0"
-        encap:="encap"
-        fou:="fou"
-        esport:= "encap-sport"
-        edport:= "encap-dport"
-        port:="port"
-        ipproto:="ipproto"
-        greP:="47"
+        //encap:="encap"
+        //fou:="fou"
+        //esport:= "encap-sport"
+        //edport:= "encap-dport"
+        //port:="port"
+        //ipproto:="ipproto"
+        //greP:="47"
         
-        cmd:=exec.Command(app, link, add, vx, typee, gretap, remote, src, local, dst, encap, fou, esport, strconv.Itoa(dport), edport, strconv.Itoa(sport))
+        cmd:=exec.Command(app, link, add, vx, typee, gretap, remote, src, local, dst)
         cmd.CombinedOutput()
         cmd=exec.Command(app, link, set, vx, up)
         cmd.CombinedOutput()
         cmd=exec.Command(app, link, set, vx, master, br0)
-        cmd.CombinedOutput()
-        cmd=exec.Command(app, fou, add, port, strconv.Itoa(dport), ipproto, greP)
         cmd.CombinedOutput()
         fmt.Println("Tunnel finish")
         x = x+1
